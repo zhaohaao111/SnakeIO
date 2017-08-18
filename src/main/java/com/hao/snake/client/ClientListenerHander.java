da00108e408c3b5eed2befd5bea715d6551ec2d6
zhao_hao@foxmail.com <ainiwuhui131>
2017-08-18 09:18:40 +0800

    线程锁优化

--- src/main/java/com/hao/snake/client/ClientListenerHander.java
+++ src/main/java/com/hao/snake/client/ClientListenerHander.java
@@ -12,52 +12,51 @@ import com.hao.snake.listener.ClientListener;
 
 public class ClientListenerHander implements ClientListener {
 	
-	JSONObject result = new JSONObject();
+	
 
 	@Override
 	public void playJoin(Skin skin, PrintWriter out) {
-		synchronized (result) {
-			result.put("type", MsgType.INIT);
-			result.put("skinIndex", skin.getIndex());
-	    	out.println(result.toJSONString());
-	    	out.flush();
-	    	result.clear();
-		}
+		JSONObject result = new JSONObject();
+		result.put("type", MsgType.INIT);
+		result.put("skinIndex", skin.getIndex());
+		out.println(result.toJSONString());
+		out.flush();
+		result.clear();
+		result = null;
 		
 	}
 
 	@Override
 	public void eatFood(int index, PrintWriter out) {
-		synchronized (result) {
-			result.put("type", MsgType.EAT_FOOD);
-			result.put("index", index);
-	    	out.println(result.toJSONString());
-	    	out.flush();
-	    	result.clear();
-		}
-		
+		JSONObject result = new JSONObject();
+		result.put("type", MsgType.EAT_FOOD);
+		result.put("index", index);
+		out.println(result.toJSONString());
+		out.flush();
+		result.clear();
+		result = null;
 	}
 
 	@Override
 	public void beforePaint(PrintWriter out) {
-		synchronized (result) {
-			result.put("type", MsgType.GET_SNAKE);
-			out.println(result.toJSONString());
-			out.flush();
-			result.clear();
-		}
+		JSONObject result = new JSONObject();
+		result.put("type", MsgType.GET_SNAKE);
+		out.println(result.toJSONString());
+		out.flush();
+		result.clear();
+		result = null;
 	}
 
 	@Override
 	public void afterPaint(PrintWriter out, Snake snake) {
-		synchronized (result) {
-			result.put("type", MsgType.UPDATE_SNAKE);
-			Node head = snake.getBody().getFirst();
-			result.put("head", JSONObject.toJSONString(head));
-			out.println(result.toJSONString());
-			out.flush();
-			result.clear();
-		}
+		JSONObject result = new JSONObject();
+		result.put("type", MsgType.UPDATE_SNAKE);
+		Node head = snake.getBody().getFirst();
+		result.put("head", JSONObject.toJSONString(head));
+		out.println(result.toJSONString());
+		out.flush();
+		result.clear();
+		result = null;
 	}
 
 	
